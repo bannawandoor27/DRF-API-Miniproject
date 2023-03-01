c1fe189313746e8d7507163c2fd7997ad99b3566
hasanul banna <bannawandoor@gmail.com>
2023-03-01 10:06:15 +0530

    model changed in user app

--- users/models.py
+++ users/models.py
@@ -3,16 +3,16 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    name = models.CharField(max_length=255)
-    mobile_number = models.IntegerField()
+    # name = models.CharField(max_length=255)
+    # mobile_number = models.IntegerField()
     email = models.EmailField(unique=True,max_length=255)
     password = models.CharField(max_length=100)
-    date_of_birth = models.DateField(max_length=8)
+    # date_of_birth = models.DateField(max_length=8)
     profile_picture = models.ImageField(upload_to='profile_pictures',blank=True,null=True)
 
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = [ 'username','mobile_number','password','date_of_birth','name','profile_picture']
+    REQUIRED_FIELDS = [ 'username','mobile_number','password','date_of_birth','name',]
 
 
     
--- users/urls.py
+++ users/urls.py
@@ -5,9 +5,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('signup/',Signup.as_view(),name='signup'),
-    path('login/',Login.as_view(),name='login'),
+    path('signin/',Login.as_view(),name='login'),
     path('user/',UserView.as_view(),name='user'),
-    path('logout/',Logout.as_view(),name='logout'), 
+    path('signout/',Logout.as_view(),name='logout'), 
 
 ]
 
